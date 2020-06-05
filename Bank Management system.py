9422e00fc6f27ca9c12f461fb03ddae60529abc2
RupeshMax <rupesh.maxpain@gmail.com>
2020-06-05 22:28:41 +0530

    Updated

--- Bank Management system.py
+++ Bank Management system.py
@@ -11,9 +11,13 @@ class Account:
         self.accno=sno+1
         self.name=input("Applicant Name:")
         self.phone=int(input("Phone Number:"))
-        self.mode=input("saving or current account(s/c):")
-        self.bal=int(input("Amount (minimum=1000):"))
-        print("Account Created Successfully!...\n***Your Account Number is",self.accno,"***")
+        self.mode=input("Saving or Current account(s/c):")
+        self.bal=int(input("Initial Amount [minimum=1000]:"))
+        if self.bal<1000:
+            print("\nPlease Deposit above minimum value")
+            self.bal=int(input("Initial Amount [minimum=1000]:"))
+            
+        print("\nAccount Created Successfully!...\n\n\t***Your Account Number is",self.accno,"***\n")
 
     def storing(self):
         sheet.append((self.accno,self.name,self.phone,self.mode,self.bal))
@@ -26,7 +30,7 @@ def Close_account(accno):
         if sheet.cell(row=i,column=1).value==accno:
             sheet.delete_rows(idx=i)
             flag=1
-            print("Your Account successfully closed!...")
+            print("\n\t***Your Account successfully closed!...***")
     if flag==0:
         print("please check your Account Number!...")
         if input("do you want to continue ?(y/n)")=='y':
@@ -49,7 +53,9 @@ def checkbalance(accno):
         if input()=='y':
             accno=int(input("Enter the correct Account Number"))
             checkbalance(accno)
-    
+
+
+
 def withdraw(accno):
     max_row=sheet.max_row
     max_column=sheet.max_column
@@ -61,14 +67,17 @@ def withdraw(accno):
             amount=int(input("Enter the withdraw amount :"))
             if amount < balance:
                 sheet[str(chr(64+max_column))+str(i)]=balance-amount
+                print("\n\t ***your a/c no.{} has been debited for Rs.{}.00 ***\n".format(accno,amount))
             else:
-                print("Insufficient balance!..")
+                print("Insufficient balance!..\n")
                 
     if flag==0:
         print("please check your Account Number!..\ndo you want to continue(y/n)")
         if input()=='y':
             accno=int(input("Enter the correct Account Number"))
             withdraw(accno)
+
+
 
 def deposit(accno):
     max_row=sheet.max_row
@@ -79,19 +88,45 @@ def deposit(accno):
             flag=1
             balance=sheet[str(chr(64+max_column))+str(i)].value
             amount=int(input("Enter the deposit amount :"))
-            if amount < balance:
-                sheet[str(chr(64+max_column))+str(i)]=balance+amount
+            sheet[str(chr(64+max_column))+str(i)]=balance+amount
+            print("\n\t ***your a/c no.{} has been credited for Rs.{}.00 ***\n".format(accno,amount))
             
     if flag==0:
         print("please check your Account Number!..\ndo you want to continue(y/n)")
         if input()=='y':
             accno=int(input("Enter the correct Account Number"))
             deposit(accno)
-    
-    
+
+
+def modify(accno):
+    max_row=sheet.max_row
+    max_column=sheet.max_column
+    flag=0
+    for i in range(2,max_row+1):
+        if sheet.cell(row=i,column=1).value==accno:
+            key=''
+            print("your Name =",sheet[str(chr(64+2))+str(i)].value)
+            key=(input("want to change (yes or no)? :")).lower()
+            if key=='yes':
+                sheet[str(chr(64+2))+str(i)]=(input("Enter your name :"))
+
+            print("your Phone Number =",sheet[str(chr(64+3))+str(i)].value)
+            key=(input("want to change (yes or no)? :")).lower()
+            if key=='yes':
+                sheet[str(chr(64+3))+str(i)]=int(input("Enter your Phone Number :"))
+                 
+            
+            flag=1
+            print("\n")
+    if flag==0:
+        print("please check your Account Number!..\ndo you want to continue(y/n)")
+        if input()=='y':
+            accno=int(input("Enter the correct Account Number"))
+            checkbalance(accno)
+
             
 def displaying(sheet):
-    print("\n")
+    print("\tBank Database\n")
     max_row=sheet.max_row
     max_column=sheet.max_column
     for i in range(1,max_row+1):
@@ -110,10 +145,11 @@ print("\tBank Management System")
 
 while True:
     print("----"*10+"\nchoose your option :")
-    print("\t1. Create Account\n\t2. Check Balance\n\t3. Deposit\n\t4. Withdraw\n\t5. Modify an Account\n\t6. close an Account\n\t7. Admin Login\n\t8. Exit")
+    print("\t1. Create New Account\n\t2. Check Balance\n\t3. Deposit\n\t4. Withdraw\n\t5. Modify an Account\n\t6. close an Account\n\t7. Admin Login\n\t8. Exit")
     x=int(input("Enter your choosen number: "))
     print("----"*10+"\n")
     if x==1:
+        print("\t Account Opening Form\n")
         account=Account()
         max_row=sheet.max_row
         accno=0
@@ -125,30 +161,44 @@ while True:
         wb.save('database.xlsx')
 
     elif x==2:
+        print("\t Checking Balance\n")
         accno=int(input("Enter the your Account Number : "))
         checkbalance(accno)
+        print("\n")
 
     elif x==3:
+        print("\t Deposit Money\n")
         accno=int(input("Enter the your Account Number : "))
         deposit(accno)
         
+        
     elif x==4:
+        print("\t Withdraw Money\n")
         accno=int(input("Enter the your Account Number : "))
         withdraw(accno)
         
+    elif x==5:
+        print("\t Update Account\n")
+        accno=int(input("Enter the your Account Number : "))
+        modify(accno)
+        
+        
     elif x==6:
+        print("\t Closing Account\n")
         accno=int(input("Enter the your Account Number : "))
         Close_account(accno)
         wb.save('database.xlsx')
         
     elif x==7:
-        print("\nchoose your option :")
-        print("\t1. Customer database\n\t2. Check Balance")
+        print("----"*10+"\nchoose your option :")
+        print("\t1. Customer database\n")
         y=int(input("Enter your choosen number: "))
+        print("----"*10+"\n")
         if y==1:
             displaying(sheet)
         
     elif x==8:
-        print("Thank you")
+        print("\t***Thank you***")
         break
-        
+    
+wb.save('database.xlsx')
